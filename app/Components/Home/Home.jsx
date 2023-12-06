3afbd6b06016c925dd0c183726b2c9be3ad1f3a3
salamanilyas <salamanilyas101@gmail.com>
2023-12-06 21:07:17 +0500

    add-home-file

---
+++ app/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <div className='home h-screen'  >
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
