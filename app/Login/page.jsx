f33fc0746a44dd4c7552b7dce438d4ec927ca1bd
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 16:44:35 +0500

    connect-frontend-to-backend

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -1,7 +1,14 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    
+useEffect(()=>{
+
+},[])
   return (
     <div>
         <section className="bg-gray-50 dark:bg-gray-900">
@@ -15,7 +22,7 @@ const page = () => {
               <div className="space-y-4 md:space-y-6">
                   <div>
                       <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                      <input type="email" name="email" onChange={()=>{}} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
                   </div>
                   <div>
                       <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
