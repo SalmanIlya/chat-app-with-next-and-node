6400de6364c1bb79c4d04656e2088d5afdaac0cb
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 16:45:12 +0500

    get-user

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+"use client"
 import React from 'react'
 import Home from './Components/Home/Home'
+import { useSelector } from 'react-redux'
 const page = () => {
-
+const user=useSelector((state)=>state.User.User)
+console.log("USER:",user);
   return (
     <div>
       <Home/>
