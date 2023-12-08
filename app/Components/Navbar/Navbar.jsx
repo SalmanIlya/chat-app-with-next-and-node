0935d9554fa0421633943f47568eaa83947ec188
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 16:44:53 +0500

    small-update

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 import React, { useState } from 'react'
 import { RiChatSmile3Line } from "react-icons/ri";
 const Navbar = () => {
-   const [user, setuser] = useState("salaman ilyas")
+   const [user, setuser] = useState("")
    const [link,setLink]=useState("/")
    const pathname=usePathname()
    console.log(pathname==="/" ? "hello world":"not");
@@ -21,7 +21,7 @@ const Navbar = () => {
         <h1 className='flex flex-row justify-center items-center text-3xl py-4'> <RiChatSmile3Line/> <span> me Chat app</span> </h1>
         <div className='flex flex-row justify-center items-center pb-2 cursor-pointer hover:underline'>
             <p className='mx-5'>{user}</p>
-            <Link href={link}   onClick={logouthandler}>Log out</Link>
+            <Link href={"/Login"}   onClick={logouthandler}>Login</Link>
         </div>
     </div>
   )
