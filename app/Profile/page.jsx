8211eb3283574b57ccd8253e65063715343baaf2
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 22:06:17 +0500

    create-profile-page

---
+++ app/Profile/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className='h-screen'>
+            Profile
+        </div>
+    </div>
+  )
+}
+
+export default page
