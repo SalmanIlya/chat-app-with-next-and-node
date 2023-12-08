0f1533f86d271997c6090d1e6872d39bdca636db
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 22:06:35 +0500

    small-modification

--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -96,11 +96,14 @@ setLink("/")
                   />
                 </div>
                
-                <Link href={link} onClick={()=>{
+                <button  onClick={()=>{
                     registerUser()
                 }} className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                  Sign up
-                </Link>
+                 <Link href={link}>
+                 Sign up
+                 
+                 </Link>
+                </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
                   <button
