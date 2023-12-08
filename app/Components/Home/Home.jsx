7449bbc593f19a6ff67348c320dd7baebf82b37a
salamanilyas <salamanilyas101@gmail.com>
2023-12-08 21:49:08 +0500

    add-chat-app

--- app/Components/Home/Home.jsx
+++ app/Components/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-
+import "./Style.css"
+import Chat from '../Chatbox/Chat';
+import { useSelector } from 'react-redux';
+import Link from 'next/link';
 const Home = () => {
+  const user=useSelector((state)=>state.User.User)
   return (
     <div>
         <Navbar />
-        <div className='home h-screen'  >
+        <div className='home h-screen flex flex-col justify-center items-center'  >
+       {
+!user.UserName ? <div>
+  <div className='flex flex-col justify-center items-center'>
+    <h1 className=' text-2xl'>Please Login First</h1>
+    <Link href="/Login" className='bg-blue-700 text-white px-5 py-2 mt-3 rounded-lg'>Login</Link>
+  </div>
 
+</div>:
+<Chat/>
+
+       }   
         </div>
     </div>
   )
