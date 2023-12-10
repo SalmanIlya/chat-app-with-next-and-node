0577faa86112f574afa6288744cdcf482e3a582d
salamanilyas <salamanilyas101@gmail.com>
2023-12-10 15:48:38 +0500

    Update page.jsx

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -4,22 +4,34 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getUser } from "../Store/User";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const page = () => {
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
-  const [link, setlink] = useState("")
-  const dispatch=useDispatch()
-const LoginHandler=async()=>{
-axios.post("http://localhost:5000/api/user/login",{Email,Password}).then((res)=>{
-    dispatch(getUser(res.data))
-    setlink("/")
-}).catch((err)=>{
-    console.log(err);
-})
-}
+  const [link, setlink] = useState("");
+  const dispatch = useDispatch();
+  const LoginHandler = async () => {
+    if (!Email && !Password) {
+      toast.error(" all fields are required");
+    } else {
+      axios
+        .post("http://localhost:5000/api/user/login", { Email, Password })
+        .then((res) => {
+          setlink("/");
+
+          dispatch(getUser(res.data));
+
+          toast.success("Login Successfully please wait a few second");
+        })
+        .catch((err) => {
+          toast.error(err.message);
+        });
+    }
+  };
   return (
     <div>
+      <ToastContainer />
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -60,18 +72,25 @@ axios.post("http://localhost:5000/api/user/login",{Email,Password}).then((res)=>
                     name="password"
                     id="password"
                     value={Password}
-                    onChange={(e)=>{setPassword(e.target.value)}}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
+                <Link href={link}>
+                  <button
+                    onClick={() => {
+                      LoginHandler();
+                    }}
+                    className="w-full mt-5 text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  >
+                    Sign in
+                  </button>
+                </Link>
 
-                <button onClick={()=>{LoginHandler()}} className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                  <Link href={link}>
-                  Sign in
-                  </Link> 
-                </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
                   <Link
