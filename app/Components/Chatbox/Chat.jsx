a13b6ed181d8c201fd0cc4fed24b0042433e35fb
salamanilyas <salamanilyas101@gmail.com>
2023-12-10 15:46:08 +0500

    connect-wth-backend-with-socket.io

--- app/Components/Chatbox/Chat.jsx
+++ app/Components/Chatbox/Chat.jsx
@@ -1,83 +1,113 @@
-import React, { useEffect, useState } from 'react'
-import { IoMdCall,IoIosVideocam  } from "react-icons/io";
+import React, { useEffect, useState } from "react";
 import { IoSend } from "react-icons/io5";
-import SocketIo from 'socket.io-client'
-import "./Style.css"
-import { useSelector } from 'react-redux';
-const endpoint='http://localhost:5000';
+import SocketIo from "socket.io-client";
+import "./Style.css";
+import { useSelector } from "react-redux";
+import ChatBox from "./ChatBox";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ScrollToBottom from "react-scroll-to-bottom";
+const endpoint = "http://localhost:5000";
 let Socket;
+
 const Chat = () => {
-  const [message, setmessage] = useState("")
-  const [messages, setmessages] = useState([])
-  const [id, setid] = useState("")
-  console.log(messages);
-  // console.log(id);
-    const User=useSelector((state)=>state.User.User)
-    const send=()=>{
- Socket.emit("message",{id,message})
- setmessage("")
-       }
-  
-    useEffect(() => {
-       Socket=SocketIo(endpoint,{transports:["websocket"]})
-      Socket.on("connect",()=>{
-        alert("Connected");
-        setid(Socket.id)
-  
-      })
-      Socket.emit("joined",{User})
-   
+  const [message, setmessage] = useState("");
+  const [messages, setmessages] = useState([]);
+  const [id, setid] = useState("");
 
+  const User = useSelector((state) => state.User.User);
 
-      Socket.on("wellcome",(data)=>{
-        console.log(data);
-      })
-      Socket.on("userJoin",(data)=>{
-        console.log(data.message);
-      })
-      Socket.on("Leave",(data)=>{
-console.log(data);
-      })
-     
-    return  ()=>{
- 
-      
-      Socket.emit("disconnected")
-      Socket.off()
-    }
-    }, [])
-   useEffect(()=>{
-    Socket.on("sendmessage",(data)=>{
-   setmessages((oldmassages)=>[...oldmassages,data])
+  useEffect(() => {
+    Socket = SocketIo(endpoint, { transports: ["websocket"] });
+    Socket.on("connect", () => {
+      toast.success("Connected");
+      setid(Socket.id);
+    });
+    Socket.emit("joined", { User });
 
-   return ()=>{
-    Socket.off()
-    // Socket.emit("disconnected")
-   }
-    })
-   },[])
-    
+    Socket.on("wellcome", (data) => {
+      toast.success(`${data.user} : ${data.message}`);
+    });
+    Socket.on("userJoin", (data) => {
+      toast.success(`${data.user} : ${data.message}`);
+    });
+    Socket.on("Leave", (data) => {
+      toast.info(`${data.user} : ${data.message}`);
+    });
+
+    return () => {
+      Socket.emit("disconnected");
+      Socket.off();
+    };
+  }, []);
+  const send = () => {
+    Socket.emit("message", { id, message });
+    setmessage("");
+  };
+  useEffect(() => {
+    Socket.on("sendmessage", (data) => {
+      setmessages((oldmassages) => [...oldmassages, data]);
+
+      return () => {
+        Socket.off();
+      };
+    });
+  }, []);
+
   return (
     <div>
-                  
-<div className='chat-box  bg-gray-50 relative'>
-{/* top */}
-<div className='top flex flex-row justify-between items-center  text-white px-5 py-4 '>
-  <h1>{User.UserName}</h1>
-  <div className='flex flex-row'>
-   < IoMdCall className='mx-3' />
-   <IoIosVideocam className='mx-3'/>
-  </div>
-</div>
-{/* center */}
-{/* bottom  */}
-<div className='w-full absolute bottom-0 flex flex-row h-12 '>
-  <input type="text" value={message} onChange={(e)=>{setmessage(e.target.value)}} placeholder=' Massage' className=' w-full outline-none border-none bg-gray-200 px-5' />
-  <button onClick={send} className='w-20 btn flex flex-col justify-center items-center'><IoSend className='icon' /></button>
-</div>
-</div>
-    </div>
-  )
-}
+      <div className="chat-box  bg-gray-50 ">
+        <ToastContainer />
 
-export default Chat
+        {/* top */}
+        <div className="top flex flex-row justify-between items-center  text-white px-5 py-4 ">
+          <h1>{User.UserName}</h1>
+          <div></div>
+        </div>
+        {/* center */}
+        {messages.length === 0 ? (
+          <div className="chatbox  flex flex-row justify-center items-center text-4xl text-gray-400">
+            <h1 className="text-center">
+              hi <span>😊</span> WellCome <br /> to the chat
+            </h1>
+          </div>
+        ) : (
+          <ScrollToBottom className="chatbox box ">
+            {messages.map((msg, userid) => {
+              return (
+                <ChatBox
+                  key={msg.id}
+                  user={msg.id === id ? "You" : msg.user}
+                  massage={msg}
+                  id={userid}
+                  classs={msg.id === id ? "Right" : "Left"}
+                />
+              );
+            })}
+          </ScrollToBottom>
+        )}
+
+        {/* bottom  */}
+        <div className="w-full  flex flex-row h-12  ">
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => {
+              setmessage(e.target.value);
+            }}
+            placeholder=" Massage"
+            className=" w-full outline-none border-none bg-gray-200 px-5"
+          />
+          <button
+            onClick={send}
+            className="w-20 btn flex flex-col justify-center items-center"
+          >
+            <IoSend className="icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
