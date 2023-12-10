b51a710b340ead6c751f624c301775637dc04953
salamanilyas <salamanilyas101@gmail.com>
2023-12-10 15:50:07 +0500

    connect-frontend-wtih-backend-with-axios

--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -1,38 +1,45 @@
-
-"use client"
+"use client";
 import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
-import {getUser} from "../Store/User"
+import { getUser } from "../Store/User";
 import { useDispatch } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const page = () => {
-    const dispatch=useDispatch()
+  const dispatch = useDispatch();
   const [UserName, setUserName] = useState("");
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
-  const [link,setLink]=useState("")
-const [User, setUser] = useState({})
-console.log(User);
-const userdata={
-   UserName,Email,Password
-}
-// useEffect(()=>{
-//     dispatch(getUser(User))
-// },[])
+  const [link, setLink] = useState("");
+  const userdata = {
+    UserName,
+    Email,
+    Password,
+  };
+  const registerUser = async () => {
+    if (!UserName && !Email && !Password) {
+      toast.error(" all fields are required");
+    } else {
+      if(Password.length<8){
+        toast.error(" password must be 8 Characters");
+      }else{
 
-const registerUser=async()=>{
-    await axios.post("http://localhost:5000/api/user/create",userdata).then((res)=>{
-setUser(res.data)
-dispatch(getUser(res.data))
-setLink("/")
-    }).catch((err)=>{
-        console.log(err);
-    })
-    
-}
+      await axios
+        .post("http://localhost:5000/api/user/create", userdata)
+        .then((res) => {
+          dispatch(getUser(res.data));
+          setLink("/");
+        })
+        .catch((err) => {
+          toast.error(err.message);
+        });
+    }}
+  };
 
   return (
     <div>
+      <ToastContainer/>
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -43,7 +50,7 @@ setLink("/")
               <div className="space-y-4 md:space-y-6">
                 <div>
                   <label
-                    for="name"
+                    htmlFor="name"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Full name
@@ -53,7 +60,9 @@ setLink("/")
                     name="name"
                     id="name"
                     value={UserName}
-                    onChange={(e)=>{setUserName(e.target.value)}}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Full name  "
                     required=""
@@ -61,7 +70,7 @@ setLink("/")
                 </div>
                 <div>
                   <label
-                    for="email"
+                    htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Your email
@@ -71,7 +80,9 @@ setLink("/")
                     name="email"
                     id="email"
                     value={Email}
-                    onChange={(e)=>{setEmail(e.target.value)}}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required=""
@@ -79,7 +90,7 @@ setLink("/")
                 </div>
                 <div>
                   <label
-                    for="password"
+                  htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Password
@@ -89,28 +100,27 @@ setLink("/")
                     name="password"
                     id="password"
                     value={Password}
-                    onChange={(e)=>{setPassword(e.target.value)}}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
                     placeholder="••••••••"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
                 </div>
-               
-                <button  onClick={()=>{
-                    registerUser()
-                }} className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                 <Link href={link}>
-                 Sign up
-                 
-                 </Link>
+
+                <button
+                  onClick={() => {
+                    registerUser();
+                  }}
+                  className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  <Link href={link}>Sign up</Link>
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
-                  <button
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    <Link href={"/Login"}>  Sign in</Link>
-                  
+                  <button className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    <Link href={"/Login"}> Sign in</Link>
                   </button>
                 </p>
               </div>
