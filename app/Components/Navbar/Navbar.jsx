f3708ddcbc540fe6861d4ba240f2923ced233382
salamanilyas <salamanilyas101@gmail.com>
2023-12-21 00:20:09 +0500

    make-navbar-responsive

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
   return (
     <div className="navbar text-white">
       <div className="flex flex-row justify-between items-center px-10 py-5 ">
-        <h1 className="flex flex-row justify-center items-center text-3xl ">
+        <h1 className="logo flex flex-row justify-center items-center text-lg  md:text-3xl lg:text-3xl 2xl:text-3xl xl:text-3xl sm:text-3xl ">
           {" "}
           <RiChatSmile3Line /> <span>Chat me </span>{" "}
         </h1>
-        <div>
+        <div >
           {LoginUser.UserName ? (
-            <div className="flex flex-row justify-center items-center">
+            <div className=" nav flex flex-row justify-center items-center text-xs  md:text-base lg:text-base 2xl:text-base xl:text-base sm:text-base ">
               <Link href={"/"} className="mx-3 hover:underline">
                 Home
               </Link>
